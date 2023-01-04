ff629098488fc2a0a2cc749165a4007ef2385142
govindmit <govind@mangoitsolutions.com>
2023-01-04 18:41:25 +0530

    change in config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,21 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Varun@123",
+//   DB: "propter",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "Varun@123",
+  PASSWORD: "V!vEk@321@123",
   DB: "propter",
   dialect: "mysql",
   pool: {
