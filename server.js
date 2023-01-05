89acc02e39e49fe6fb4d7a67dac2c7cdda8d1129
govindmit <govind@mangoitsolutions.com>
2023-01-05 10:38:10 +0530

    change in config

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
